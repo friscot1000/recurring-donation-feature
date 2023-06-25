06cb48527aa3c7fe0d7013eab697f5905236cdb8
Francisco Contreras <friscot1000@aol.com>
2023-06-24 22:50:31 -0700

    adding gfm-recurring executable adding input.txt file adding readme

--- DonationManager.py
+++ DonationManager.py
@@ -1,50 +1,46 @@
 import sys
-
+from collections import OrderedDict
 from Campaign import Campaign
 from Donor import Donor
 
 
 class DonationManager:
-    def __init__(self):
+    def __init__(self, filename):
         self.campaigns = {}
         self.donors = {}
+        self.filename = filename
 
     def user_input(self):
-        for user_input in sys.stdin:
-            user_input = user_input.strip().lower()
-            if 'add campaign' in user_input:
-                self.add_campaign(user_input)
-            elif 'add donor' in user_input:
-                self.add_donor(user_input)
-            elif 'donate' in user_input:
-                self.donate(user_input)
-            elif 'exit' == user_input:
-                self.write_donors()
-                self.write_campaigns()
-                break
-            else:
-                print(f'Input: {user_input}')
+        with open(self.filename, "r") as data:
+            for line in data:
+                text_input = line.lower().strip()
+                if 'add campaign' in text_input:
+                    self.add_campaign(text_input)
+                elif 'add donor' in text_input:
+                    self.add_donor(text_input)
+                elif 'donate' in text_input:
+                    self.donate(text_input)
+            self.write_donors()
+            self.write_campaigns()
+            print("")
 
     def add_campaign(self, user_input):
         campaign_data = user_input.split()
         campaign_name = campaign_data[2]
         if campaign_name not in self.campaigns:
             self.campaigns[campaign_name] = Campaign(campaign_name)
-            print(f"Campaign {campaign_name} added")
 
     def add_donor(self, user_input):
         donor_data = user_input.split()
         donor = donor_data[2]
-        print(donor)
         if donor not in self.donors:
             self.donors[donor] = Donor(donor)
-            print(f"Donor {donor} added")
 
     def donate(self, user_input):
         donation_data = user_input.split()
         donor_name = donation_data[1]
         campaign_name = donation_data[2]
-        amount = int(donation_data[3])
+        amount = int(donation_data[3].strip('$'))
 
         if campaign_name in self.campaigns:
             self.campaigns[campaign_name].make_donation(amount)
@@ -54,21 +50,22 @@ class DonationManager:
             self.donors[donor_name].donation(amount)
         else:
             print("Donor does not exist.")
-        print("Thank you " + donor_name + " for your donation of " + str(amount) + ' benefiting ' + campaign_name)
 
     def write_donors(self):
+        sorted_donors = OrderedDict(sorted(self.donors.items()))
         with open('output/output.txt', 'w') as output:
             output.write('Donors:\n')
-            for donor in self.donors.values():
+            for donor in sorted_donors.values():
                 output.write(str(donor) + '\n')
 
     def write_campaigns(self):
+        sorted_campaigns = OrderedDict(sorted(self.campaigns.items()))
         with open('output/output.txt', 'a') as output:
             output.write('\nCampaigns:\n')
-            for campaign in self.campaigns.values():
+            for campaign in sorted_campaigns.values():
                 output.write(str(campaign) + '\n')
 
 
 if __name__ == '__main__':
-    donation_manager = DonationManager()
+    donation_manager = DonationManager(sys.argv[1])
     donation_manager.user_input()
